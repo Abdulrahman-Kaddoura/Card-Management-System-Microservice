a5e75dc701c19c772f3123ac735e73caaccc81a8
abdkad123 <akaddoura123@gmail.com>
2026-02-06 14:49:09 +0200

    feat: added api documentation for swagger

--- src/main/java/com/springProject/boot/controllers/AccountCardController.java
+++ src/main/java/com/springProject/boot/controllers/AccountCardController.java
@@ -1,24 +1,45 @@
 package com.springProject.boot.controllers;
 
 import com.springProject.boot.services.AccountCardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/account-card")
+@Tag(name = "Account Card", description = "Operations to link accounts and cards")
 public class AccountCardController {
+
     @Autowired
     AccountCardService accountCardService;
 
     @PostMapping("/link-account-card/{accountId}/{cardId}")
-    public ResponseEntity<UUID> linkAccountCard(@PathVariable UUID accountId, @PathVariable UUID cardId) throws Exception {
-        return new ResponseEntity<>(accountCardService.linkAccountCard(accountId,cardId), HttpStatus.CREATED);
+    @Operation(
+            summary = "Link Account with Card",
+            description = "Links an existing account with an existing card by their UUIDs"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Account successfully linked to the card"),
+            @ApiResponse(responseCode = "400", description = "Invalid account or card ID provided"),
+            @ApiResponse(responseCode = "404", description = "Account or Card not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<UUID> linkAccountCard(
+            @Parameter(description = "UUID of the account to link", required = true)
+            @PathVariable UUID accountId,
+
+            @Parameter(description = "UUID of the card to link", required = true)
+            @PathVariable UUID cardId
+    ) throws Exception {
+        UUID linkedId = accountCardService.linkAccountCard(accountId, cardId);
+        return new ResponseEntity<>(linkedId, HttpStatus.CREATED);
     }
 }
